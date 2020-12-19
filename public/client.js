a9bcc820764d485b5b2eca4df7f010c98913b4f2
TheSowrdBreaker <kunalguptadev007@gmail.com>
2020-12-19 20:38:59 +0530

    Added turn server

--- public/client.js
+++ public/client.js
@@ -31,6 +31,11 @@ const iceServers = {
     { urls: 'stun:stun2.l.google.com:19302' },
     { urls: 'stun:stun3.l.google.com:19302' },
     { urls: 'stun:stun4.l.google.com:19302' },
+    {
+      url: 'turn:192.158.29.39:3478?transport=udp',
+      credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+      username: '28224511:1379330808'
+    },
   ],
 }
 
